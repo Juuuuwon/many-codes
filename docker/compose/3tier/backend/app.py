2fd9bc3e38d46389e984bbe049c98ed5e693c30b
test <test@test.com>
2024-06-10 19:15:52 +0900

    .

---
+++ docker/compose/3tier/backend/app.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import pymysql
+
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    conn = pymysql.connect(
+        host='db',   
+        user='testuser', 
+        password='testpassword', 
+        database='testdb'
+    )
+
+    try:
+        with conn.cursor() as cursor:
+            sql = "SELECT * from test;"
+            cursor.execute(sql)
+    finally:
+        conn.close()
+
+    return {"message": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
